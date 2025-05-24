1fabfbf2800fefc43ef0d24d1b93f76cbd2287be
Alayo Shamsudeen <sosweetsparke@gmail.com>
2025-05-24 18:57:08 +0100

    resolving compatibility of versions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 export default defineConfig({
   plugins: [react(), visualizer({ open: true })],
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-bootstrap',
+      '@hello-pangea/dnd'
+    ]
+  },
   build: {
     rollupOptions: {
       output: {
